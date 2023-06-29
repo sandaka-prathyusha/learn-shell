027969f424e45adb76b2a061fbc97bcd24b6e5b7
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-06-29 16:19:58 +0530

    Color code

--- 02-echo.sh
+++ 02-echo.sh
@@ -1,1 +1,10 @@
 echo hello world
+
+echo -e "\e[31m RED COLOR\e[0m"
+echo -e "\e[32m GREEN COLOR\e[0m"
+echo -e "\e[33m YELLOW COLOR\e[0m"
+echo -e "\e[34m BLUE COLOR\e[0m"
+echo -e "\e[35m MAGENTA COLOR\e[0m"
+echo -e "\e[36m CYAN COLOR\e[0m"
+echo -e "\e[31m RED COLOR"
+NANDU
