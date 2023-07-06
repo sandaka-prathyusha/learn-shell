8a4c488c642427c8aad5d23f18f0585d11935e2c
Prathyusha <prathyusha.sandaka96@gmail.com>
2023-07-06 13:42:36 +0530

    movie reveiw

---
+++ example-imdb.sh
@@ -0,0 +1,17 @@
+#Get rating
+# > 7 - Good
+# < 7 - Average
+
+input=$1
+if [ -z "$input" ]; then
+   echo Input Movie Name Missing
+   exit
+fi
+
+percent=$curl -s https://www.themoviedb.org/movie/955555-3|grep "user_score_chart"| grep  data-percent|xargs -n1|grep data-percent|awk -F = '{print $2}'
+
+if [ "$percent" -ge 70 ]; then
+   echo Good
+else
+   echo Average
+fi
